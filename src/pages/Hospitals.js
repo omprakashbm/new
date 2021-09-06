115cbe949d8260149b52115b9549335fb9f47ac1
sarbajeetchhetri <sarbajeetchhetri36@gmail.com>
2021-09-06 10:43:22 +0545

    feat: added Dialog modal and Filter Equpment Data.

--- src/pages/Hospitals.js
+++ src/pages/Hospitals.js
@@ -3,6 +3,7 @@ import { DataGrid } from "@material-ui/data-grid";
 import { fetchURL } from "../apiComponents/FetchComponent";
 import { LanguageContext } from "../App";
 import Typography from "@material-ui/core/Typography";
+import { Dialog } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -99,6 +100,25 @@ export default function Hospital() {
   let language = useContext(LanguageContext);
   const [hospitalData, setHospitalData] = useState({ results: [] });
   const [equipemntData, setEquipemntData] = useState({ results: [] });
+  const [selected, setSelected] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [filterData, setFilterData] = useState(columnsEquipment);
+
+  // console.log("selected", selected);
+  // console.log("equipemntData", equipemntData);
+  // console.log("filterData", filterData);
+
+  const handleClose = (value) => {
+    setOpen(false);
+    // setSelected(value);
+  };
+
+  useEffect(() => {
+    setFilterData(
+      equipemntData.results.filter((dt) => dt?.hospital === selected?.id)
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected]);
 
   useEffect(() => {
     const getData = async () => {
@@ -149,6 +169,10 @@ export default function Hospital() {
           {hospitalData && (
             <div style={{ height: 600, width: "100%" }}>
               <DataGrid
+                onRowClick={(item) => {
+                  setSelected(item);
+                  setOpen(true);
+                }}
                 rows={hospitalData.results}
                 columns={columnsHospital}
                 pageSize={10}
@@ -157,21 +181,48 @@ export default function Hospital() {
           )}
         </CardContent>
       </Card>
+
       <Card className={classes.root}>
-        <CardContent className={classes.root}>
-          <Typography gutterBottom variant="h5" component="h2" color="primary">
-            Equipments Data
-          </Typography>
-          {equipemntData && (
-            <div style={{ height: 600, width: "100%" }}>
-              <DataGrid
-                rows={equipemntData.results}
-                columns={columnsEquipment}
-                pageSize={10}
-              />
-            </div>
-          )}
-        </CardContent>
+        <Dialog
+          onClose={handleClose}
+          // aria-labelledby="simple-dialog-title"
+          open={open}
+          fullWidth={true}
+          maxWidth={"lg"}
+        >
+          <CardContent className={classes.root}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h2"
+              color="primary"
+            >
+              Equipments Data {selected?.row?.name}
+            </Typography>
+            {/* {equipemntData && (
+              <div style={{ height: 600, width: "100%" }}>
+                <DataGrid
+                  rows={equipemntData.results}
+                  columns={columnsEquipment}
+                  pageSize={10}
+                />
+              </div>
+            )} */}
+
+            {equipemntData ? (
+              <div style={{ height: 400, width: "100%" }}>
+                <DataGrid
+                  rows={filterData}
+                  // rows={equipemntData.results}
+                  columns={columnsEquipment}
+                  pageSize={10}
+                />
+              </div>
+            ) : (
+              <tr>No data Available</tr>
+            )}
+          </CardContent>
+        </Dialog>
       </Card>
     </div>
   );
